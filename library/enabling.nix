156a434fea42ba2080449f3f48229d13da12b2b4
GitHver? <hver.vef@gmail.com>
2025-01-04 19:49:32 +0000

    more enabling options

--- library/enabling.nix
+++ library/enabling.nix
@@ -2,4 +2,14 @@
   enabled = { enable = true; };
   disabled = { enable = false; };
   enableBash = { enable = true; enableBashIntegration = true; };
+  enableZsh = { enable = true; enableZshIntegration = true; };
+  enableFish = { enable = true; enableFishIntegration = true; };
+  enableNushell = { enable = true; enableNushellIntegration = true; };
+  enableAllShells = {
+    enable = true;
+    enableBashIntegration = true;
+    enableZshIntegration = true;
+    enableFishIntegration = true;
+    enableNushellIntegration = true;    
+  };
 }
