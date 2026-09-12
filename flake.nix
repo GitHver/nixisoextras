e44a01c25678a5f20cd6291adc37dd5002a420fb
GitHver? <hver.vef@gmail.com>
2026-09-12 06:32:12 +0200

    system fix 2?

--- flake.nix
+++ flake.nix
@@ -63,11 +63,11 @@
     # to overlay overrides to existing packages in the with custom options. This
     # alloes you to apply your own patches or build flags with out needing to
     # maintain a fork of nixpkgs or adding a third party repository.
-    # overlays = {
-    #   default = (final: prev: {
-    #     sputnix = self.pkgs.${prev.system};
-    #   });
-    # };
+    overlays = {
+      default = (final: prev: {
+        sputnix = self.pkgs.stdenv.hostPlatform.${prev.system};
+      });
+    };
 
     #====<< Nix Development Shells >>==========================================>
     # Development shells `nix develop` are ephemeral environments where you can
